bbfc7c14991f0743fed6a18e806dbb83816c92be
SamanthaNguyen1 <>
2021-05-08 23:15:59 +0000

    Javascript syntax

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ function displayMessage () {
   alert('hello')
 }
 
-// changes the font of "Julie Li"
+// changes the font of "Samantha Nguyen"
 document.getElementById('font').addEventListener('click', changeFont)
 function changeFont () {
   document.getElementById('name').style.fontFamily = 'Brush-Script-MT ,cursive'
 }
 
-// changes the size of "Julie Li"
+// changes the size of "Samantha Nguyen"
 document.getElementById('size').addEventListener('click', changeSize)
 function changeSize () {
   document.getElementById('name').style.fontSize = '150%'
 }
 
-// changes color of "Julie Li"
-document.getElementById('color').addEventListener('click', changeColor)
+// changes color of "Samantha Nguyen"
+document.getElementById('colour').addEventListener('click', changeColor)
 function changeColor () {
-  document.getElementById('name').style.color = 'purple'
+  document.getElementById('name').style.color = '#9966cc'
 }
